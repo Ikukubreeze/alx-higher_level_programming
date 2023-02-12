91e52408e49d184def3030f2a5956ad192920817
Ikukubreeze <legiondesignsed@gmail.com>
2023-02-12 02:17:37 -0800

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
+
+    __nb_objects = 0
